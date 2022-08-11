8dd200a22c63234b735c747b55b2c4bd4d174fd1
Garance Clausse <garanceclausse@msn.com>
2022-08-11 14:06:26 +0200

    initial commit

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gclausse <gclausse@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/11 13:54:27 by gclausse          #+#    #+#             */
+/*   Updated: 2022/08/11 13:54:27 by gclausse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>>
+
+class Fixed
+{
+private:
+	int _value;
+	static const int _fract_bits;
+
+public:
+	Fixed();
+	Fixed(const Fixed& copy);
+	~Fixed();
+};
+
+Fixed::Fixed(/* args */)
+{
+}
+
+Fixed::~Fixed()
+{
+}
---
+++ ex00/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gclausse <gclausse@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/11 13:53:47 by gclausse          #+#    #+#             */
+/*   Updated: 2022/08/11 13:53:47 by gclausse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main( void ) {
+Fixed a;
+Fixed b( a );
+Fixed c;
+c = b;
+
+std::cout << a.getRawBits() << std::endl;
+std::cout << b.getRawBits() << std::endl;
+std::cout << c.getRawBits() << std::endl;
+
+return 0;
+}
